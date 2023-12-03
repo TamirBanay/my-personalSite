1549dc94419c06787ad2665f264dcfcc9edc61b5
Tamir Banay <banay9329@gmail.com>
2023-12-03 16:39:55 +0200

    add navbar

---
+++ my-personal-website/src/componets/navbar/Navbar.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { ScrollLink } from "react-scroll";
+import { Link } from "react-scroll";
+import "./Navbar.css";
+function Navbar() {
+  // Component code
+  return (
+    <nav>
+      <ul>
+        <li className="name">
+          {/* <a href={myCV} download="Tamir Banay CV.pdf"> */}
+          Tamir Banay {/* </a> */}
+        </li>
+        <div className="routers">
+          <Link to="projects" smooth={true} duration={500}>
+            <li>Projects</li>
+          </Link>
+
+          <Link to="about-contaoner" smooth={true} duration={500}>
+            <li>About</li>
+          </Link>
+          <Link to="main-skills-contaoner" smooth={true} duration={500}>
+            <li>Contact</li>
+          </Link>
+        </div>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
