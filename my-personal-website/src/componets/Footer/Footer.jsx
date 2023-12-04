e25601f6b97c952d16b47417db85b127adad2c92
Tamir Banay <banay9329@gmail.com>
2023-12-04 18:53:49 +0200

    add responsive footer

--- my-personal-website/src/componets/Footer/Footer.jsx
+++ my-personal-website/src/componets/Footer/Footer.jsx
@@ -7,11 +7,13 @@ function Footer() {
   return (
     <div>
       {document.documentElement.clientWidth > 480 ? (
-        <div className="footerImg-container" >
+        <div className="footerImg-container">
           <img src={footerImg} className="footerImg" />
         </div>
       ) : (
-        <img className="footerImgMobile" src={footerImgMobile} />
+        <div className="footerImg-container-mobile">
+          <img className="footerImgMobile" src={footerImgMobile} />
+        </div>
       )}
     </div>
   );
