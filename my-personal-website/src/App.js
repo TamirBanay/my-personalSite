73e3e322b6353397789c4611170dea1b844edca1
Tamir Banay <banay9329@gmail.com>
2023-12-11 19:31:48 +0200

    add a swipe to close the menu

--- my-personal-website/src/App.js
+++ my-personal-website/src/App.js
@@ -15,14 +15,41 @@ import {
   useRecoilValue,
 } from "recoil";
 import { _menuIsOpen } from "./services/atom";
+import React, { useState, useRef } from "react";
 
 function App() {
   const [menuIsOpen, setMenuIsOpen] = useRecoilState(_menuIsOpen);
+  const touchStartRef = useRef(0);
+  const touchEndRef = useRef(0);
+  const handleTouchStart = (e) => {
+    touchStartRef.current = e.targetTouches[0].clientX;
+  };
 
+  const handleTouchMove = (e) => {
+    touchEndRef.current = e.targetTouches[0].clientX;
+  };
+
+  const handleTouchEnd = () => {
+    const swipeThreshold = 30; // Minimum distance of the swipe to trigger action
+    const swipeRight =
+      touchEndRef.current > touchStartRef.current + swipeThreshold;
+    const swipeLeft =
+      touchStartRef.current > touchEndRef.current + swipeThreshold;
+
+    if (swipeRight && menuIsOpen) {
+      setMenuIsOpen(false); // Close the menu on swipe right
+    } else if (swipeLeft && !menuIsOpen) {
+      setMenuIsOpen(true); // Open the menu on swipe left
+    }
+  };
   return (
-    <div className="App">
+    <div
+      onTouchStart={handleTouchStart}
+      onTouchMove={handleTouchMove}
+      onTouchEnd={handleTouchEnd}
+      className="App"
+    >
       {menuIsOpen && <MobileMenu />}
-
       <div className={menuIsOpen ? "hidden-background" : ""}>
         <Navbar />
         <Intro />
